aa3515c223b1ca9db303af846f33783c05785773
Ilman <pythonilmantest@gmail.com>
2021-01-20 21:54:15 +0530

    Added API for get requests for exisiting posts

--- postapiapp/views.py
+++ postapiapp/views.py
@@ -10,40 +10,50 @@ from .serializer import PostSerializer, CustomUserSerializer
 
 # Create your views here.
 class ReadOnlyPostView(APIView):
-    """API View class for Post Read only serializer"""
+    """
+    API View class for Read only - PostSerializer
+    """
     serializer_class = PostSerializer
 
     def get(self, request):
-        """function for get request"""
+        """function for fetching posts from db and send to frontend"""
         post_data = [{"user_name": post_data.user_name, "post_desc": post_data.post_desc, "time_stamp": post_data.time_stamp}
-        for post_data in Post.objects.all()]
+                     for post_data in Post.objects.all()]
         return Response(post_data)
 
 
 class PostView(APIView):
-    """API View class for Post serializer"""
-    
-    permission_classes = [IsAuthenticated]
+    """
+    API View class for New Posts - PostSerializer
+    """
+
     serializer_class = PostSerializer
 
-    def post(self, request): 
-        """function to add new posts"""
-        serializer = PostSerializer(data=request.data) 
-        if serializer.is_valid(raise_exception=True): 
-            serializer.save() 
-            return  Response(serializer.data)
+    def post(self, request):
+        """
+        function to add handle new posts request
+        """
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+
 
 class CustomUserSignUp(APIView):
-    """API View for signup request"""
+    """
+    API View for signup request
+    """
 
     serializer_class = CustomUserSerializer
+
     def post(self, request):
+        """
+        function to handle new signup request
+        """
+
         serializer = CustomUserSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             USERPROFILE = serializer.save()
             if USERPROFILE:
                 return Response(status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-
